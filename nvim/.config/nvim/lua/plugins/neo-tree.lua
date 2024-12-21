4bbec34fae90d3b743d0e432e7bd4ed746a408de
Shaun Simons <shaunsimons93@gmail.com>
2024-12-21 16:30:09 +1100

    wallpapers; nvim debugger; vim-test; neotree config

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,17 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-  end
-  }
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+    require("neo-tree").setup({
+      filesystem = {
+        follow_current_file = true,
+        hijack_netrw_behavior = "open_default",
+        use_libuv_file_watcher = true,
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignore = false,
+        },
+      },
+    })
+  end,
+}
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -9,6 +9,11 @@ vim.g.maplocalleader = "\\"
 vim.opt.relativenumber = true
 vim.opt.number = true
 
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-f>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
 vim.keymap.set("n", "<C-d>", "<C-d>zzzv")
 vim.keymap.set("n", "<C-u>", "<C-u>zzzv")
 vim.keymap.set("n", "n", "nzzzv")
@@ -20,8 +25,8 @@ vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>")
 
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("HighlightOnYank", {}),
-	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
-	end,
+  group = vim.api.nvim_create_augroup("HighlightOnYank", {}),
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+  end,
 })
