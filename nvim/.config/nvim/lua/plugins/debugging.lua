2c08f2d7094c863c3c7df25bde3a505fd9cb88fb
Shaun Simons <simonss@RCMAC9.local>
2025-06-29 15:10:59 +1000

    neotest

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -6,19 +6,27 @@ return {
     "mfussenegger/nvim-dap-python",
   },
   config = function()
+    vim.fn.mkdir(vim.fn.stdpath("cache") .. "/dap", "p")
     local dap = require("dap")
+    dap.set_log_level("TRACE")
     local dapui = require("dapui")
     local dap_python = require("dap-python")
-    dap_python.setup(".venv/bin/python") -- Adjust the path to your Python interpreter
-    dap.adapters.python = {
+    dap_python.setup("venv/bin/python")
+    dap.adapters.python_server = {
       type = 'server',
       host = '127.0.0.1',
       port = 5678,
     }
+    dap.adapters.python_exec = {
+      type = 'executable',
+      command = "venv/bin/python",
+      args = { "-m", "debugpy.adapter" },
+      justMyCode = false,
+    }
 
     dap.configurations.python = {
       {
-        type = 'python',
+        type = 'python_server',
         request = 'attach',
         name = 'Attach to server',
         connect = {
@@ -29,40 +37,33 @@ return {
         justMyCode = false,
         pathMappings = {
           {
-            localRoot = vim.fn.getcwd(), -- your project root
+            localRoot = vim.fn.getcwd(),
             remoteRoot = vim.fn.getcwd(),
           },
         },
-      }
+      },
+      {
+        name = "Launch (local)",
+        type = "python_exec",
+        request = "launch",
+        program = "${file}",
+        pythonPath = "venv/bin/python",
+      },
     }
 
-    dapui.setup(
-      {
-        highlights = {
-          -- Apply the background to these groups
-          Scope = "DapUIBackground",
-          Variable = "DapUIBackground",
-          Breakpoints = "DapUIBackground",
-          Stacks = "DapUIBackground",
-          Watches = "DapUIBackground",
-          REPL = "DapUIBackground",
-          Console = "DapUIBackground",
-        },
-      }
-    )
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
+    dapui.setup()
+
+    dap.listeners.before.event_terminated["dapui_config"] = function()
       dapui.close()
     end
-    dap.listeners.before.event_exited.dapui_config = function()
+    dap.listeners.before.event_exited["dapui_config"] = function()
       dapui.close()
+    end
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      vim.schedule(dapui.open)
     end
     vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
     vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-  end,
+    vim.keymap.set("n", "<Leader>du", dapui.toggle, {})
+  end
 }
--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -1,6 +1,1 @@
-return {
-  "vim-test/vim-test",
-  vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>"),
-  vim.keymap.set("n", "<leader>ta", ":TestSuite<CR>"),
-  vim.keymap.set("n", "<leader>tf", ":TestFile<CR>"),
-}
+return {}
