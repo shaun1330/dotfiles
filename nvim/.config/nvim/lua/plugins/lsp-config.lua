2555cfbdd063d4faf4d4ce17fee9a071b1b342af
Shaun Simons <shaunsimons93@gmail.com>
2025-07-02 20:47:26 +1000

    lsp configs

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,77 +1,63 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-    opts = {
-      ensure_installed = {
-        "mypy",
-        "ruff"
-      },
-    }
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls", "bashls", "biome", "pylsp",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup({
+				ensure_installed = {
+					"mypy",
+					"ruff",
+				},
+			})
+		end,
+		opts = {},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"bashls",
+					"biome",
+					"pylsp",
+					"ruff",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.bashls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.biome.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ruff.setup({
-        settings = {
-          pyright = {
-            -- Using Ruff's import organizer
-            disableOrganizeImports = true
-          },
-          python = {
-            analysis = {
-              -- Ignore all files for analsis to exclusively use Ruff for linting
-              ignore = { '*' }
-            }
-          }
-        }
-      })
-      lspconfig.pylsp.setup({
-        settings = {
-          pylsp = {
-            plugings = {
-              mypy = {
-                enabled = true,
-                live_mode = true,
-              },
-            },
-          }
-        }
-      })
-
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.bashls.setup({ capabilities = capabilities })
+			lspconfig.biome.setup({ capabilities = capabilities })
+			lspconfig.ruff.setup({ capabilities = capabilities })
+			lspconfig.pylsp.setup({ capabilities = capabilities,
+				settings = {
+					pylsp = {
+						plugins = {
+							mypy = {
+								enabled = true,
+								live_mode = false,
+								strict = false,
+								overrides = { "--config-file", "mypy.ini" },
+							},
+							pycodestyle = {
+								maxLineLength = 120,
+							},
+						},
+					},
+				},
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
