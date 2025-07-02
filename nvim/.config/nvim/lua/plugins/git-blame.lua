4c4f974169e7ab3cf78b8e1d4b8c6683453a6171
Shaun Simons <simonss@RCMAC9.local>
2025-07-02 11:54:45 +1000

    new git blame plugin

--- nvim/.config/nvim/lua/plugins/git-blame.lua
+++ nvim/.config/nvim/lua/plugins/git-blame.lua
@@ -1,13 +1,1 @@
-return {
-  "f-person/git-blame.nvim",
-  event = "VeryLazy",
-  opts = {
-    enable = true,
-    message_template = "<author> <date> <summary>",
-    date_format = "%Y-%m-%d %H:%M:%S",
-    virtual_text_column = 1,
-  },
-  keys = {
-    { "<leader>gb", "<cmd>GitBlameToggle<cr>", desc = "GitBlameToggle" },
- }
-}
+return { }
