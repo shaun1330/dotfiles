4c03b05038e700144868ff5ac03a6b1f233cbe54
Shaun Simons <simonss@RCMAC9.local>
2025-07-11 21:28:45 +1000

    treesitter context plugin

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,5 @@
 return {
+  {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
@@ -12,4 +13,17 @@ return {
 		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 		vim.opt.foldlevel = 99
 	end,
+ },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup({
+        enable = true,
+        max_lines = 0, -- No limit on the number of lines
+        trim_scope = "outer", -- Show context from outer scopes
+        min_window_height = 0, -- No minimum window height
+        mode = "cursor", -- Show context based on cursor position
+      })
+    end,
+  }
 }
